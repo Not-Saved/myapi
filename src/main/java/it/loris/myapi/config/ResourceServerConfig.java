7f62051ac027a95f16f607d9cea180552aabfade
Loris Corradin <45518034+Not-Saved@users.noreply.github.com>
2019-05-02 21:41:43 +0200

    Fixed a minor problem

--- src/main/java/it/loris/myapi/config/ResourceServerConfig.java
+++ src/main/java/it/loris/myapi/config/ResourceServerConfig.java
@@ -18,6 +18,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").authenticated();
+                .antMatchers("/**").authenticated();
     }
 }
