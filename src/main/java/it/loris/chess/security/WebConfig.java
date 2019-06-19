cd06c34d27654445ad5099577adbcbbf0aeb8c43
Not-Saved <corradin.loris@gmail.com>
2019-06-19 21:32:37 +0200

    React: Website development continues, react router, redux, sagas etc.

--- src/main/java/it/loris/chess/security/WebConfig.java
+++ src/main/java/it/loris/chess/security/WebConfig.java
@@ -1,12 +1,8 @@
 package it.loris.chess.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
